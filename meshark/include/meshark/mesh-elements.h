b23c54e7af33070420c246bfb06086246b60283f
Sun Jiahao <engraftmenew@gamil.com>
2025-12-26 15:46:32 +0800

    finish OutgoingHalfEdgeRange in task1

--- meshark/include/meshark/mesh-elements.h
+++ meshark/include/meshark/mesh-elements.h
@@ -9,227 +9,272 @@
 #include <meshark/element-set.h>
 #include <cassert>
 
-namespace meshark {
-struct HalfEdgeElement {
-  explicit HalfEdgeElement(int index) : index(index) {}
-  Vertex tip;
-  Vertex tail;
-  HalfEdge next;
-  HalfEdge twin;
-  Face face;
-  Edge edge;
- protected:
-  template<typename Derived>
-  friend
-  struct HalfEdgeMesh;
-  int index;
-};
+namespace meshark
+{
+  struct HalfEdgeElement
+  {
+    explicit HalfEdgeElement(int index) : index(index) {}
+    Vertex tip;
+    Vertex tail;
+    HalfEdge next;
+    HalfEdge twin;
+    Face face;
+    Edge edge;
 
-inline HalfEdge nullHalfEdge() {
-  return mystl::make_observer<HalfEdgeElement>(nullptr);
-}
-
-struct EdgeElement {
-  explicit EdgeElement(int index) : index(index) {}
-
-  [[nodiscard]] HalfEdge halfEdge() const {
-    return he;
-  }
-
-  HalfEdge &halfEdge() {
-    return he;
-  }
-
-  [[nodiscard]] Vertex firstVertex() const {
-    return he->tip;
-  }
-
-  [[nodiscard]] Vertex secondVertex() const {
-    return he->tail;
-  }
-
- protected:
-  template<typename Derived>
-  friend
-  struct HalfEdgeMesh;
-  template<typename T>
-  friend
-  struct EdgeData;
-  HalfEdge he;
-  int index;
-};
-
-inline Edge nullEdge() {
-  return mystl::make_observer<EdgeElement>(nullptr);
-}
-
-struct FaceElement {
- protected:
-  struct BoundaryLoop {
-    explicit BoundaryLoop(HalfEdge start) : start(start) {}
-
-    struct Iterator {
-      Iterator &operator++() {
-        it = it->next;
-        if (it == start) it = static_cast<HalfEdge>(nullptr);
-        return *this;
-      }
-
-      HalfEdge operator*() const {
-        return it;
-      }
-
-      bool operator==(const Iterator &other) const {
-        return it == other.it;
-      }
-
-      HalfEdge start;
-      HalfEdge it;
-    };
-
-    HalfEdge start;
-
-    [[nodiscard]] Iterator begin() const {
-      return {
-          .start = start,
-          .it = start,
-      };
-    }
-
-    [[nodiscard]] Iterator end() const {
-      return {
-          .start = start,
-          .it = static_cast<HalfEdge>(nullptr)
-      };
-    }
+  protected:
+    template <typename Derived>
+    friend struct HalfEdgeMesh;
+    int index;
   };
 
- public:
-  explicit FaceElement(int index) : index(index) {}
-
-  [[nodiscard]] HalfEdge halfEdge() const {
-    return he;
+  inline HalfEdge nullHalfEdge()
+  {
+    return mystl::make_observer<HalfEdgeElement>(nullptr);
   }
 
-  HalfEdge &halfEdge() {
-    return he;
-  }
+  struct EdgeElement
+  {
+    explicit EdgeElement(int index) : index(index) {}
 
-  [[nodiscard]] BoundaryLoop boundaryHalfEdges() const {
-    return BoundaryLoop(he);
-  }
-
-  [[nodiscard]] Vertex vertex() const {
-    return he->tip;
-  }
-
-  [[nodiscard]] Edge edge() const {
-    return he->edge;
-  }
-
- protected:
-  template<typename Derived>
-  friend
-  struct HalfEdgeMesh;
-  template<typename T>
-  friend
-  struct FaceData;
-  HalfEdge he;
-  int index;
-};
-inline Face nullFace() {
-  return mystl::make_observer<FaceElement>(nullptr);
-}
-struct VertexElement {
- private:
-  struct OutgoingHalfEdgeRange {
-    explicit OutgoingHalfEdgeRange(HalfEdge start) : start(start) {}
-
-    struct Iterator {
-      Iterator &operator++() {
-        // TODO: implement operator++ for OutgoingHalfEdgeRange::Iterator
-
-        return *this;
-      }
-
-      HalfEdge operator*() const {
-        return it;
-      }
-
-      bool operator==(const Iterator &other) const {
-        return it == other.it;
-      }
-
-      HalfEdge start;
-      HalfEdge it;
-    };
-
-    HalfEdge start;
-
-    [[nodiscard]] Iterator begin() const {
-      return {
-          .start = start,
-          .it = start,
-      };
+    [[nodiscard]] HalfEdge halfEdge() const
+    {
+      return he;
     }
 
-    [[nodiscard]] Iterator end() const {
-      return {
-          .start = start,
-          .it = static_cast<HalfEdge>(nullptr)
-      };
+    HalfEdge &halfEdge()
+    {
+      return he;
     }
+
+    [[nodiscard]] Vertex firstVertex() const
+    {
+      return he->tip;
+    }
+
+    [[nodiscard]] Vertex secondVertex() const
+    {
+      return he->tail;
+    }
+
+  protected:
+    template <typename Derived>
+    friend struct HalfEdgeMesh;
+    template <typename T>
+    friend struct EdgeData;
+    HalfEdge he;
+    int index;
   };
 
- public:
-  explicit VertexElement(int index) : index(index) {}
-
-  [[nodiscard]] HalfEdge halfEdge() const {
-    return he;
+  inline Edge nullEdge()
+  {
+    return mystl::make_observer<EdgeElement>(nullptr);
   }
 
-  HalfEdge &halfEdge() {
-    return he;
-  }
+  struct FaceElement
+  {
+  protected:
+    struct BoundaryLoop
+    {
+      explicit BoundaryLoop(HalfEdge start) : start(start) {}
 
-  [[nodiscard]] OutgoingHalfEdgeRange outgoingHalfEdges() const {
-    return OutgoingHalfEdgeRange(he);
-  }
+      struct Iterator
+      {
+        Iterator &operator++()
+        {
+          it = it->next;
+          if (it == start)
+            it = static_cast<HalfEdge>(nullptr);
+          return *this;
+        }
 
-  [[nodiscard]] int degree() const {
-    int deg = 0;
-    for (auto h : outgoingHalfEdges())
-      deg++;
-    return deg;
-  }
+        HalfEdge operator*() const
+        {
+          return it;
+        }
 
-  [[nodiscard]] VertexSet adjacentVertices() const {
-    std::vector<Vertex> adj_vertices;
-    adj_vertices.reserve(degree());
-    for (auto h : outgoingHalfEdges())
-      adj_vertices.emplace_back(h->tip);
-    return VertexSet(adj_vertices);
-  }
+        bool operator==(const Iterator &other) const
+        {
+          return it == other.it;
+        }
 
-  [[nodiscard]] std::optional<HalfEdge> halfEdgeTo(Vertex v) const {
-    for (auto h : outgoingHalfEdges()) {
-      if (h->tip == v) return h;
+        HalfEdge start;
+        HalfEdge it;
+      };
+
+      HalfEdge start;
+
+      [[nodiscard]] Iterator begin() const
+      {
+        return {
+            .start = start,
+            .it = start,
+        };
+      }
+
+      [[nodiscard]] Iterator end() const
+      {
+        return {
+            .start = start,
+            .it = static_cast<HalfEdge>(nullptr)};
+      }
+    };
+
+  public:
+    explicit FaceElement(int index) : index(index) {}
+
+    [[nodiscard]] HalfEdge halfEdge() const
+    {
+      return he;
     }
-    return std::nullopt;
-  }
 
- protected:
-  template<typename Derived>
-  friend
-  struct HalfEdgeMesh;
-  template<typename T>
-  friend
-  struct VertexData;
-  HalfEdge he;
-  int index;
-};
-inline Vertex nullVertex() {
-  return mystl::make_observer<VertexElement>(nullptr);
+    HalfEdge &halfEdge()
+    {
+      return he;
+    }
+
+    [[nodiscard]] BoundaryLoop boundaryHalfEdges() const
+    {
+      return BoundaryLoop(he);
+    }
+
+    [[nodiscard]] Vertex vertex() const
+    {
+      return he->tip;
+    }
+
+    [[nodiscard]] Edge edge() const
+    {
+      return he->edge;
+    }
+
+  protected:
+    template <typename Derived>
+    friend struct HalfEdgeMesh;
+    template <typename T>
+    friend struct FaceData;
+    HalfEdge he;
+    int index;
+  };
+  inline Face nullFace()
+  {
+    return mystl::make_observer<FaceElement>(nullptr);
+  }
+  struct VertexElement
+  {
+  private:
+    struct OutgoingHalfEdgeRange
+    {
+      explicit OutgoingHalfEdgeRange(HalfEdge start) : start(start) {}
+
+      struct Iterator
+      {
+        Iterator &operator++()
+        {
+          // TODO: implement operator++ for OutgoingHalfEdgeRange::Iterator
+          // For an outgoing halfedge h (tail == v), the next outgoing halfedge
+          // around v is: h = h->twin->next.
+          if (!it)
+            return *this; // already at end
+          assert(it->twin);
+          HalfEdge next_he = it->twin->next;
+          it = next_he;
+
+          // When we circle back to start, mark as end.
+          if (it == start || !it)
+          {
+            it = static_cast<HalfEdge>(nullptr);
+          }
+          return *this;
+        }
+
+        HalfEdge operator*() const
+        {
+          return it;
+        }
+
+        bool operator==(const Iterator &other) const
+        {
+          return it == other.it;
+        }
+
+        HalfEdge start;
+        HalfEdge it;
+      };
+
+      HalfEdge start;
+
+      [[nodiscard]] Iterator begin() const
+      {
+        return {
+            .start = start,
+            .it = start,
+        };
+      }
+
+      [[nodiscard]] Iterator end() const
+      {
+        return {
+            .start = start,
+            .it = static_cast<HalfEdge>(nullptr)};
+      }
+    };
+
+  public:
+    explicit VertexElement(int index) : index(index) {}
+
+    [[nodiscard]] HalfEdge halfEdge() const
+    {
+      return he;
+    }
+
+    HalfEdge &halfEdge()
+    {
+      return he;
+    }
+
+    [[nodiscard]] OutgoingHalfEdgeRange outgoingHalfEdges() const
+    {
+      return OutgoingHalfEdgeRange(he);
+    }
+
+    [[nodiscard]] int degree() const
+    {
+      int deg = 0;
+      for (auto h : outgoingHalfEdges())
+        deg++;
+      return deg;
+    }
+
+    [[nodiscard]] VertexSet adjacentVertices() const
+    {
+      std::vector<Vertex> adj_vertices;
+      adj_vertices.reserve(degree());
+      for (auto h : outgoingHalfEdges())
+        adj_vertices.emplace_back(h->tip);
+      return VertexSet(adj_vertices);
+    }
+
+    [[nodiscard]] std::optional<HalfEdge> halfEdgeTo(Vertex v) const
+    {
+      for (auto h : outgoingHalfEdges())
+      {
+        if (h->tip == v)
+          return h;
+      }
+      return std::nullopt;
+    }
+
+  protected:
+    template <typename Derived>
+    friend struct HalfEdgeMesh;
+    template <typename T>
+    friend struct VertexData;
+    HalfEdge he;
+    int index;
+  };
+  inline Vertex nullVertex()
+  {
+    return mystl::make_observer<VertexElement>(nullptr);
+  }
 }
-}
-#endif //MESHSIMPLIFICATION_MESHARK_INCLUDE_MESHARK_MESH_ELEMENTS_H_
+#endif // MESHSIMPLIFICATION_MESHARK_INCLUDE_MESHARK_MESH_ELEMENTS_H_
